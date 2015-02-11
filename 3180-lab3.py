b8a405060034cb23dfc82c798c31b4fa624d1811
Hevon <hevongordon@gmail.com>
2015-02-11 11:21:50 -0500

    this is the script to send message

---
+++ 3180-lab3.py
@@ -0,0 +1,54 @@
+import smtplib
+
+fromaddr = 'hevongordon.com'
+
+toaddr = 'hevongordon.com'
+
+message = """From: {} <{}>
+
+To: {} <{}>
+
+Subject: {}
+
+{}
+
+"""
+
+fromname="hevongordon"
+fromaddr="hevongordon@gmail.com"
+toname="hevongordon"
+toaddr="hevongordon@gmail.com"
+subject="stuff"
+msg="this is a message from me to me"
+
+messagetosend = message.format(
+
+ fromname,
+
+ fromaddr,
+
+ toname,
+
+ toaddr,
+
+ subject,
+
+ msg)
+
+# Credentials (if needed)
+
+username = 'hevongordon@gmail.com'
+
+password = 'kzleowzlnezoomdy'
+
+# The actual mail send
+
+server = smtplib.SMTP('smtp.gmail.com:587')
+
+server.starttls()
+
+server.login(username,password)
+
+server.sendmail(fromaddr, toaddrs, messagetosend)
+
+server.quit()
